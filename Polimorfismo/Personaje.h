87e03f6bd2539dc2ffa4329ee829de3ce8a8f337
Turupawn <ahmed.hn.43@gmail.com>
2015-07-29 11:22:30 -0600

    inicio polimofismo

---
+++ Polimorfismo/Personaje.h
@@ -0,0 +1,16 @@
+#ifndef PERSONAJE_H
+#define PERSONAJE_H
+
+
+class Personaje
+{
+    public:
+        int vida;
+        int ataque;
+        Personaje();
+        virtual ~Personaje();
+    protected:
+    private:
+};
+
+#endif // PERSONAJE_H
